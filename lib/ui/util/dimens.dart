ce87a2f8ef5bed79afa950cce03d279bba8e917f
Tolga Bolatcan <tolga.bolatcan@adesso.com.tr>
2021-06-21 23:58:25 +0300

    generic tab is added.

---
+++ lib/ui/util/dimens.dart
@@ -0,0 +1,17 @@
+// Dimens
+class Dimens {
+  static const double ELEVATION = 8;
+  static const double NONE = 0;
+
+  static const double TABHEIGHT = 140;
+
+  static const double XXSMALL = 2;
+  static const double XSMALL = 4;
+  static const double SMALL = 8;
+  static const double MEDIUM = 16;
+  static const double LARGE = 24;
+  static const double XLARGE = 24;
+  static const double XXLARGE = 32;
+
+
+}
