8de6a9fb74c061aba72587626ec916133415be26
Batu Simsek <bsimsek159@gmail.com>
2021-08-09 11:17:18 +0300

    platfrom channel, ios test impl

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,27 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "flutter_base_okr/sample",
+                                                  binaryMessenger: controller.binaryMessenger)
+    channel.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+          guard call.method == "getPairedBluetoothDevices" else {
+            result(FlutterMethodNotImplemented)
+            return
+          }
+          self?.getPairedBluetoothDevices(result: result)
+        })
+
+        GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func getPairedBluetoothDevices(result: FlutterResult) {
+        //TODO: Not implemented yet.
+        let testDictionary: [[String: String]] = [["deviceName" : "Hello From IOS",
+                                                "address" : "Test",
+                                                "alias" : "Hello From IOS"]];
+        result(testDictionary)
+    }
 }
